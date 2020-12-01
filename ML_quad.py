57f77848d68ade0f35122dc0ac7d1f23626796a8
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-12-01 10:23:32 +0000

    Add files via upload

---
+++ ML_quad.py
@@ -0,0 +1,352 @@
+# +
+# # !pip install multi-imbalance
+
+# +
+import pandas as pd
+import numpy as np
+import csv
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import tree
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+
+from sklearn import metrics  
+from sklearn.metrics import roc_curve, auc, roc_auc_score, f1_score
+from sklearn.metrics import recall_score, precision_score, accuracy_score
+from sklearn.metrics import confusion_matrix  
+
+# +
+#feats =   ['HR_mean','HR_std','SD_mean','SD_std','pNN50','TINN','RMSSD','LF','HF','ULF','VLF','LFHF','Total_power','label']
+#path = 'wesad_paper_ppg/data_merged_normalized_window_300.csv'
+
+feats = ['HR_mean','HR_std','meanNN','SDNN','medianNN','meanSD','SDSD','RMSSD','pNN20','pNN50','TINN','LF','HF','ULF','VLF','LFHF',
+         'total_power','lfp','hfp','SD1','SD2','pA','pQ','ApEn','shanEn','D2','subject','label']
+WINDOW_SIZE = '120'
+
+NOISE = ['ens_1','bp_ens_1','bp_time_ens_1']
+
+NOISE = ['original_1', 'bp_1', 'bp_time_1']
+
+NOISE = ['bp_1', 'bp_time_1']
+subjects = [2,3,4,5,6,7,8,9,10,11,13,14,15,16,17]
+#subjects = [2,3,4,6,7,8,9,10,11,13,14,15,16,17]
+
+# +
+from imblearn.over_sampling import SMOTE
+from collections import Counter
+
+def over_sampling(X_train, y_train, X_test, y_test):
+    
+    sm = SMOTE(random_state=42)
+    
+    print("b_y_train: ", Counter(y_train))
+    print("b_y_test: ", Counter(y_test))
+    
+    X_train, y_train = sm.fit_resample(X_train, y_train)
+    X_test, y_test = sm.fit_resample(X_test, y_test)
+    
+    print("b_y_train: ", Counter(y_train))
+    print("b_y_test: ", Counter(y_test))
+    
+    return X_train, y_train, X_test, y_test
+
+
+def read_csv(path, feats, testset_num):
+    print("testset num: ",testset_num)
+    df = pd.read_csv(path, index_col = 0)
+    
+    df = df[feats]
+
+    train_df = df.loc[df['subject'] != testset_num]
+    test_df =  df.loc[df['subject'] == testset_num]
+
+    del train_df['subject']
+    del test_df['subject']
+    del df['subject']
+
+    
+    X_train = train_df.drop('label', axis=1).values
+    y_train = train_df['label'].values   
+    X_test = test_df.drop('label', axis=1).values
+    y_test = test_df['label'].values    
+    
+    return df, X_train, y_train, X_test, y_test
+# -
+
+
+
+# # Machine learning
+
+
+
+# +
+def DT_model(X_train, y_train, X_test, y_test):
+    
+    model = tree.DecisionTreeClassifier(random_state=0)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    return AUC, F1, ACC
+
+
+def RF_model(X_train, y_train, X_test, y_test):
+    
+    model = RandomForestClassifier(max_depth=4, random_state=0)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    return AUC, F1, ACC
+
+def AB_model(X_train, y_train, X_test, y_test):
+    
+    model = AdaBoostClassifier(random_state=0)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    return AUC, F1, ACC
+
+def KN_model(X_train, y_train, X_test, y_test):
+    
+    model = KNeighborsClassifier(n_neighbors=9)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    return AUC, F1, ACC
+
+def LDA_model(X_train, y_train, X_test, y_test):
+    
+    model = LinearDiscriminantAnalysis()
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    
+    return AUC, F1, ACC
+
+def SVM_model(X_train, y_train, X_test, y_test):
+    
+    model = svm.SVC()
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    
+    return AUC, F1, ACC
+
+
+def GB_model(X_train, y_train, X_test, y_test):
+    
+    model = GradientBoostingClassifier(random_state=0)
+    model.fit(X_train,y_train)
+    y_pred = model.predict(X_test)
+    
+    ACC = accuracy_score(y_test, y_pred)
+    
+    fpr,tpr, roc_auc = dict(), dict(), dict()
+    n_classes = 4
+    
+    y_pred = np.eye(n_classes)[y_pred]
+    y_test = np.eye(n_classes)[y_test]  # one-hot-vector
+    
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+    
+    AUC = np.array(list(roc_auc.values())).mean()
+    F1 = f1_score(np.argmax(y_test, axis=1), np.argmax(y_pred, axis=1), average='macro')  
+    
+    
+    return AUC, F1, ACC
+
+# +
+feats = ['HR_mean','HR_std','meanNN','SDNN','medianNN','meanSD','SDSD','RMSSD','pNN20','pNN50','TINN','LF','HF','ULF','VLF','LFHF',
+         'total_power','SD1','SD2','pA','pQ','ApEn','shanEn','D2','subject','label']       
+
+for n in NOISE:
+    
+    #path = '27_features_ppg_9/data_merged_' + n + WINDOW_SIZE + '.csv'
+    #result_path_all = 'result/BGM2/all_features_' + n + WINDOW_SIZE + '.csv'
+
+    path = '27_features_ppg_test_4/LMM/data_merged_' + n + WINDOW_SIZE + '.csv'
+    result_path_all = 'result_4/LMM/all_features_' + n + WINDOW_SIZE + '.csv'
+
+    DT_AUC, DT_F1, DT_ACC = [], [], []
+    RF_AUC, RF_F1, RF_ACC = [], [], []
+    AB_AUC, AB_F1, AB_ACC = [], [], []
+    KN_AUC, KN_F1, KN_ACC = [], [], []
+    LDA_AUC, LDA_F1, LDA_ACC = [], [], []
+    SVM_AUC, SVM_F1, SVM_ACC = [], [], []
+    GB_AUC, GB_F1, GB_ACC = [], [], []
+
+    for sub in subjects:
+    
+        df, X_train, y_train, X_test, y_test = read_csv(path, feats, sub)
+        df.fillna(0)
+        # Normalization
+        sc = StandardScaler()  
+        X_train = sc.fit_transform(X_train)  
+        X_test = sc.transform(X_test)  
+
+
+        # oversampling using SMOTE    
+        #X_train, y_train, X_test, y_test = over_sampling(X_train, y_train, X_test, y_test)
+    
+        auc_dt, f1_dt, acc_dt = DT_model(X_train, y_train, X_test, y_test)
+        auc_rf, f1_rf, acc_rf = RF_model(X_train, y_train, X_test, y_test)
+        auc_ab, f1_ab, acc_ab = AB_model(X_train, y_train, X_test, y_test)
+        auc_kn, f1_kn, acc_kn = KN_model(X_train, y_train, X_test, y_test)
+        auc_lda, f1_lda, acc_lda = LDA_model(X_train, y_train, X_test, y_test)
+        auc_svm, f1_svm, acc_svm = SVM_model(X_train, y_train, X_test, y_test)
+        auc_gb, f1_gb, acc_gb = GB_model(X_train, y_train, X_test, y_test)
+
+        DT_AUC.append(auc_dt)
+        DT_F1.append(f1_dt)
+        DT_ACC.append(acc_dt)
+        RF_AUC.append(auc_rf)
+        RF_F1.append(f1_rf)
+        RF_ACC.append(acc_rf)
+        AB_AUC.append(auc_ab)
+        AB_F1.append(f1_ab)
+        AB_ACC.append(acc_ab)
+        KN_AUC.append(auc_kn)
+        KN_F1.append(f1_kn)
+        KN_ACC.append(f1_kn)
+        LDA_AUC.append(auc_lda)
+        LDA_F1.append(f1_lda)
+        LDA_ACC.append(acc_lda)
+        SVM_AUC.append(auc_svm)
+        SVM_F1.append(f1_svm)
+        SVM_ACC.append(acc_svm)
+        GB_AUC.append(auc_gb)
+        GB_F1.append(f1_gb)
+        GB_ACC.append(acc_gb)
+    
+
+    with open(result_path_all, 'w', newline='') as file:
+        writer = csv.writer(file)
+
+        writer.writerow(['subject','S2','S3','S4','S5','S6','S7','S8','S9','S10','S11','S13','S14','S15','S16','S17','total'])
+        writer.writerow(['DT_AUC'] + DT_AUC + [np.mean(DT_AUC)])
+        writer.writerow(['RF_AUC'] + RF_AUC + [np.mean(RF_AUC)])
+        writer.writerow(['AB_AUC'] + AB_AUC + [np.mean(AB_AUC)])
+        writer.writerow(['KN_AUC'] + KN_AUC + [np.mean(KN_AUC)])
+        writer.writerow(['LDA_AUC'] + LDA_AUC + [np.mean(LDA_AUC)])
+        writer.writerow(['SVM_AUC'] + SVM_AUC + [np.mean(SVM_AUC)])
+        writer.writerow(['GB_AUC'] + GB_AUC + [np.mean(GB_AUC)])
+        writer.writerow(['DT_F1'] + DT_F1 + [np.mean(DT_F1)])
+        writer.writerow(['RF_F1'] + RF_F1 + [np.mean(RF_F1)])
+        writer.writerow(['AB_F1'] + AB_F1 + [np.mean(AB_F1)])
+        writer.writerow(['KN_F1'] + KN_F1 + [np.mean(KN_F1)])
+        writer.writerow(['LDA_F1'] + LDA_F1 + [np.mean(LDA_F1)])
+        writer.writerow(['SVM_F1'] + SVM_F1 + [np.mean(SVM_F1)])
+        writer.writerow(['GB_F1'] + GB_F1 + [np.mean(GB_F1)])
+        writer.writerow(['DT_ACC'] + DT_ACC + [np.mean(DT_ACC)])
+        writer.writerow(['RF_ACC'] + RF_ACC + [np.mean(RF_ACC)])
+        writer.writerow(['AB_ACC'] + AB_ACC + [np.mean(AB_ACC)])
+        writer.writerow(['KN_ACC'] + KN_ACC + [np.mean(KN_ACC)])
+        writer.writerow(['LDA_ACC'] + LDA_ACC + [np.mean(LDA_ACC)])
+        writer.writerow(['SVM_ACC'] + SVM_ACC + [np.mean(SVM_ACC)])
+        writer.writerow(['GB_ACC'] + GB_ACC + [np.mean(GB_ACC)])
+
+
+        file.close()
+ 
+    print("DONE: ",n)
+# -
+# #### 
+
---
+++ read_data_new_tri.py
@@ -0,0 +1,278 @@
+# -*- coding: utf-8 -*-
+# +
+import os
+import pickle
+import math
+import numpy as np
+import pandas as pd
+
+from scipy.interpolate import UnivariateSpline
+from preprocessing_tool.feature_extraction import *
+# +
+WINDOW_IN_SECONDS = 120  # 120 / 180 / 300
+
+NOISE = ['bp_time_1']
+NOISE = ['ens_1','bp_ens_1','bp_time_ens_1']
+NOISE = ['original_1','bp_1','bp_time_1']
+
+
+# +
+# E4 (wrist) Sampling Frequencies
+
+fs_dict = {'ACC': 32, 'BVP': 64, 'EDA': 4, 'TEMP': 4, 'label': 700, 'Resp': 700}
+
+label_dict = {'baseline': 1, 'stress': 2, 'amusement': 0}
+int_to_label = {1: 'baseline', 2: 'stress', 0: 'amusement'}
+    
+sec = 12
+N = fs_dict['BVP']*sec  # one block : 10 sec
+overlap = int(np.round(N * 0.02)) # overlapping length
+overlap = overlap if overlap%2 ==0 else overlap+1
+
+
+# -
+
+class SubjectData:
+
+    def __init__(self, main_path, subject_number):
+        self.name = f'S{subject_number}'
+        self.subject_keys = ['signal', 'label', 'subject']
+        self.signal_keys = ['chest', 'wrist']
+        self.chest_keys = ['ACC', 'ECG', 'EMG', 'EDA', 'Temp', 'Resp']
+        self.wrist_keys = ['ACC', 'BVP', 'EDA', 'TEMP']
+        with open(os.path.join(main_path, self.name) + '/' + self.name + '.pkl', 'rb') as file:
+            self.data = pickle.load(file, encoding='latin1')
+        self.labels = self.data['label']
+
+    def get_wrist_data(self):
+        data = self.data['signal']['wrist']
+        data.update({'Resp': self.data['signal']['chest']['Resp']})
+        return data
+
+    def get_chest_data(self):
+        return self.data['signal']['chest']
+
+    def extract_features(self):  # only wrist
+        results = \
+            {
+                key: get_statistics(self.get_wrist_data()[key].flatten(), self.labels, key)
+                for key in self.wrist_keys
+            }
+        return results
+
+
+def extract_ppg_data(e4_data_dict, labels, norm_type=None):
+    # Dataframes for each sensor type
+    df = pd.DataFrame(e4_data_dict['BVP'], columns=['BVP'])
+    label_df = pd.DataFrame(labels, columns=['label'])
+    
+
+    # Adding indices for combination due to differing sampling frequencies
+    df.index = [(1 / fs_dict['BVP']) * i for i in range(len(df))]
+    label_df.index = [(1 / fs_dict['label']) * i for i in range(len(label_df))]
+
+    # Change indices to datetime
+    df.index = pd.to_datetime(df.index, unit='s')
+    label_df.index = pd.to_datetime(label_df.index, unit='s')
+
+    df = df.join(label_df, how='outer')
+    
+    df['label'] = df['label'].fillna(method='bfill')
+    
+    df.reset_index(drop=True, inplace=True)
+    
+    if norm_type is 'std':  # 시그널 자체를 normalization
+        # std norm
+        df['BVP'] = (df['BVP'] - df['BVP'].mean()) / df['BVP'].std()
+    elif norm_type is 'minmax':
+        # minmax norm
+        df = (df - df.min()) / (df.max() - df.min())
+
+    # Groupby
+    df = df.dropna(axis=0) # nan인 행 제거
+    
+    return df
+
+
+def seperate_data_by_label(df):
+    
+    grouped = df.groupby('label')
+    baseline = grouped.get_group(1)
+    stress = grouped.get_group(2)
+    amusement = grouped.get_group(3)   
+    
+    return grouped, baseline, stress, amusement
+
+
+
+def get_samples(data, label, ma_usage):
+    global feat_names
+    global WINDOW_IN_SECONDS
+
+    samples = []
+
+    window_len = fs_dict['BVP'] * WINDOW_IN_SECONDS  # 64*60 , sliding window: 0.25 sec (60*0.25 = 15)   
+    sliding_window_len = int(fs_dict['BVP'] * WINDOW_IN_SECONDS * 0.25)
+    
+    winNum = 0
+    
+    i = 0
+    while sliding_window_len * i <= len(data) - window_len:
+        
+         # 한 윈도우에 해당하는 모든 윈도우 담기,
+        w = data[sliding_window_len * i: (sliding_window_len * i) + window_len]  
+        # Calculate stats for window
+        wstats = get_window_stats_27_features(ppg_seg=w['BVP'].tolist(), window_length = window_len, label=label, ensemble = ENSEMBLE, ma_usage=ma_usage)
+        winNum += 1
+        
+        if wstats == []:
+            i += 1
+            continue;
+        # Seperating sample and label
+        x = pd.DataFrame(wstats, index = [i])
+    
+        samples.append(x)
+        i += 1
+
+    return pd.concat(samples)
+
+
+def make_patient_data(subject_id,ma_usage):
+    global savePath
+    global WINDOW_IN_SECONDS
+    
+    temp_ths = [1.0,2.0,1.8,1.5] #temp_ths = [1.1,2.2,2.0,1.9] 
+    clean_df = pd.read_csv('clean_signal_by_rate.csv',index_col=0)
+    cycle = 15
+    
+    # Make subject data object for Sx
+    subject = SubjectData(main_path='/root/Desktop/workspace/seongsil/stress-detection/WESAD/', subject_number=subject_id)
+    
+    # Empatica E4 data
+    e4_data_dict = subject.get_wrist_data()
+
+    # norm type
+    norm_type = 'std'
+
+    df = extract_ppg_data(e4_data_dict, subject.labels, norm_type)
+    df_BVP = df.BVP
+
+
+    #여기서 signal preprocessing 
+    bp_bvp = butter_bandpassfilter(df_BVP, 0.5, 10, fs_dict['BVP'], order=2) # 0.5, 5 -> 0.5,10
+    
+    if BP:
+        df['BVP'] = bp_bvp
+        
+    if FREQ:
+        signal_one_percent = int(len(df_BVP) * 0.01)
+        print(signal_one_percent)
+        cutoff = get_cutoff(df_BVP[:signal_one_percent], fs_dict['BVP'])
+        freq_signal = compute_and_reconstruction_dft(df_BVP, fs_dict['BVP'], sec, overlap, cutoff)
+        df['BVP'] = freq_signal
+
+    if TIME:
+        fwd = moving_average(bp_bvp, size=3)
+        bwd = moving_average(bp_bvp[::-1], size=3)
+        bp_bvp = np.mean(np.vstack((fwd,bwd[::-1])), axis=0)
+        df['BVP'] = bp_bvp
+        
+        signal_01_percent = int(len(df_BVP) * 0.001)
+        print(signal_01_percent, int(clean_df.loc[subject_id]['index']))
+        clean_signal = df_BVP[int(clean_df.loc[subject_id]['index']):int(clean_df.loc[subject_id]['index'])+signal_01_percent]
+        ths = statistic_threshold(clean_signal, fs_dict['BVP'], temp_ths)
+        len_before, len_after, time_signal_index = eliminate_noise_in_time(df['BVP'].to_numpy(), fs_dict['BVP'], ths, cycle)
+    
+        df = df.iloc[time_signal_index,:]
+        df = df.reset_index(drop=True)
+        #plt.figure(figsize=(40,20))
+        #plt.plot(df['BVP'][:2000], color = 'b', linewidth=2.5)
+    
+    
+    grouped, baseline, stress, amusement = seperate_data_by_label(df)   
+    
+    
+    
+    baseline_samples = get_samples(baseline, 1, ma_usage)
+    print("bas: ",len(baseline_samples))
+    stress_samples = get_samples(stress, 2, ma_usage)
+    print("st: ",len(stress_samples))
+    amusement_samples = get_samples(amusement, 0, ma_usage)
+    print("Am: ",len(amusement_samples))
+    window_len = len(baseline_samples)+len(stress_samples)+len(amusement_samples)
+
+    all_samples = pd.concat([baseline_samples, stress_samples, amusement_samples])
+    all_samples = pd.concat([all_samples.drop('label', axis=1), pd.get_dummies(all_samples['label'])], axis=1) # get dummies로 원핫벡터로 라벨값 나타냄
+    
+    
+    all_samples.to_csv(f'{savePath}{subject_feature_path}/S{subject_id}_feats_4.csv')
+
+    # Does this save any space?
+    subject = None
+    
+    return window_len
+
+
+
+def combine_files(subjects):
+    df_list = []
+    for s in subjects:
+        df = pd.read_csv(f'{savePath}{subject_feature_path}/S{s}_feats_4.csv', index_col=0)
+        df['subject'] = s
+        df_list.append(df)
+
+    df = pd.concat(df_list)
+
+    df['label'] = (df['0'].astype(str) + df['1'].astype(str) + df['2'].astype(str)).apply(lambda x: x.index('1'))  # 1인 부분의 인덱스 반환
+    df.drop(['0', '1', '2'], axis=1, inplace=True)
+
+    df.reset_index(drop=True, inplace=True)
+
+    df.to_csv(savePath + merged_path)
+
+    counts = df['label'].value_counts()
+    print('Number of samples per class:')
+    for label, number in zip(counts.index, counts.values):
+        print(f'{int_to_label[label]}: {number}')
+
+
+# +
+total_window_len = 0
+BP, FREQ, TIME, ENSEMBLE = False, False, False, False
+subject_ids = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17]
+
+feat_names = None
+savePath = '27_features_ppg_test_3/LMM/'
+
+if not os.path.exists(savePath):
+    os.makedirs(savePath)
+
+
+for n in NOISE:
+    if 'bp' in n.split('_'):
+        BP = True
+    if 'time' in n.split('_'):
+        TIME = True
+    if 'ens' in n.split('_'):
+        ENSEMBLE = True
+        
+    print(BP, FREQ, TIME, ENSEMBLE)
+    
+
+    subject_feature_path = '/subject_feature_' + n + str(WINDOW_IN_SECONDS)
+    merged_path = '/data_merged_' + n + str(WINDOW_IN_SECONDS) +'.csv'
+    
+    if not os.path.exists(savePath + subject_feature_path):
+        os.makedirs(savePath + subject_feature_path)
+    
+        
+    for patient in subject_ids:
+        print(f'Processing data for S{patient}...')
+        window_len = make_patient_data(patient, BP)
+        total_window_len += window_len
+
+    combine_files(subject_ids)
+    print('total_Window_len: ',total_window_len)
+    print('Processing complete.', n)
+    total_window_len = 0
+
