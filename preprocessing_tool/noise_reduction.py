5d895f8aa78e101994965e16e8f1ce5fe44288a7
seongsilheo <44438752+seongsilheo@users.noreply.github.com>
2020-12-22 03:50:19 +0000

    Add files via upload

---
+++ preprocessing_tool/noise_reduction.py
@@ -0,0 +1,421 @@
+# -*- coding: utf-8 -*-
+# +
+import math
+from scipy import signal
+from scipy import fft, ifft
+from scipy import fftpack
+from scipy.stats import kurtosis, skew
+from scipy.signal import butter, lfilter
+from statsmodels.graphics import tsaplots
+from scipy import stats
+from sklearn.linear_model import LinearRegression
+
+
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+# +
+# filter
+
+
+def butter_bandpass(lowcut, highcut, fs, order=5):
+    nyq = 0.5 * fs  
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def butter_bandpassfilter(data, lowcut, highcut, fs, order=5):
+    b, a = butter_bandpass(lowcut, highcut, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
+
+def movingaverage(data, periods=4):
+    result = []
+    data_set = np.asarray(data)
+    weights = np.ones(periods) / periods
+    result = np.convolve(data_set, weights, mode='valid')
+    return result
+
+# -
+
+
+
+def detrend_signals(signals):
+    detrended_signals = []
+    X = [k for k in range(0, len(signals))]
+    X = np.reshape(X, (len(X), 1)) ## Reshapes the array into necessary format
+    model = LinearRegression() ## Defines the model
+    model.fit(X, signals) ## Fits signals to the model
+    trend = model.predict(X) ## Predicts the model's trend
+    detrend = [signals[k] - trend[k] for k in range(len(signals))] ## Removes trend from signal
+    detrended_signals.append(detrend)
+    
+    return detrended_signals[0]
+
+
+# +
+'''
+Method 1 ) local minima and maxima
+'''
+
+def threshold_peakdetection(dataset, fs):
+    
+    #print("dataset: ",dataset)
+    window = []
+    peaklist = []
+    ybeat = []
+    listpos = 0
+    mean = np.average(dataset)
+    TH_elapsed = np.ceil(0.36 * fs)
+    npeaks = 0
+    peakarray = []
+    
+    localaverage = np.average(dataset)
+    for datapoint in dataset:
+
+        if (datapoint < localaverage) and (len(window) < 1):
+            listpos += 1
+        elif (datapoint >= localaverage):
+            window.append(datapoint)
+            listpos += 1
+        else:
+            maximum = max(window)
+            beatposition = listpos - len(window) + (window.index(max(window)))
+            peaklist.append(beatposition)
+            window = []
+            listpos += 1
+
+            
+    ## Ignore if the previous peak was within 360 ms interval becasuse it is T-wave
+    for val in peaklist:
+        if npeaks > 0:
+            prev_peak = peaklist[npeaks - 1]
+            elapsed = val - prev_peak
+            if elapsed > TH_elapsed:
+                peakarray.append(val)
+        else:
+            peakarray.append(val)
+            
+        npeaks += 1    
+
+
+    return peaklist
+
+
+def RR_interval(peaklist,fs):
+    RR_list = []
+    cnt = 0
+    while (cnt < (len(peaklist)-1)):
+        RR_interval = (peaklist[cnt+1] - peaklist[cnt]) # Calculate distance between beats in # of samples
+        ms_dist = ((RR_interval / fs) * 1000.0)  # Convert sample distances to ms distances (fs로 나눠서 1초단위로 거리표현 -> 1ms단위로 change) 
+        RR_list.append(ms_dist)
+        cnt += 1
+        
+    return RR_list
+
+
+def calc_heartrate(RR_list):
+    HR = []
+    heartrate_array=[]
+    window_size = 10
+
+    for val in RR_list:
+        if val > 400 and val < 1500:
+            heart_rate = 60000.0 / val #60000 ms /1 minute. time per beat(한번 beat하는데 걸리는 시간)
+            
+        # if RR-interval < .1905 seconds, heart-rate > highest recorded value, 315 BPM. Probably an error!
+        elif (val > 0 and val < 400) or val > 1500:
+            if len(HR) > 0:
+                # ... and use the mean heart-rate from the data so far:
+                heart_rate = np.mean(HR[-window_size:])
+
+            else:
+                heart_rate = 60.0
+        else:
+            # Get around divide by 0 error
+            heart_rate = 0.0
+
+        HR.append(heart_rate)
+
+    return HR
+
+
+# +
+'''
+1. Frequency view
+
+Reference:
+Sadhukhan, Deboleena, Saurabh Pal, and Madhuchhanda Mitra. 
+"PPG Noise Reduction based on Adaptive Frequency Suppression using Discrete Fourier Transform 
+for Portable Home Monitoring Applications." 
+2018 15th IEEE India Council International Conference (INDICON). IEEE, 2018.
+
+'''
+
+def FFT(block,fs):
+    fourierTransform = np.fft.fft(block)/len(block)  # divide by len(block) to normalize
+    fourierTransform = fourierTransform[range(int(len(block)/2))] # single side frequency / symmetric
+
+    tpCount = len(block)
+    values = np.arange(int(tpCount)/2)
+
+    timePeriod = tpCount / fs
+    frequencies = values/timePeriod # frequency components
+
+    '''
+    plt.figure(figsize=(40,20))
+    plt.plot(frequencies, abs(fourierTransform)) 
+
+    plt.xticks(fontsize=30)
+    plt.yticks(fontsize=30)
+    plt.xlabel("frequency(Hz)",fontsize=50)
+    plt.ylabel("FFT magnitude(mV)",fontsize=50)
+    '''
+    
+    return frequencies, fourierTransform, timePeriod
+
+
+
+#def get_cutoff(block,peak,fs):
+def get_cutoff(block,fs):
+    
+    block = np.array([item.real for item in block])
+    peak = threshold_peakdetection(block,fs)
+    hr_mean = np.mean(calc_heartrate(RR_interval(peak,fs)))
+    #low_cutoff = np.round(hr_mean / 60 - 0.2, 1)
+    low_cutoff = np.round(hr_mean / 60 - 0.6, 1) # 0.6
+    
+    
+    frequencies, fourierTransform, timePeriod = FFT(block,fs)
+    ths = max(abs(fourierTransform)) * 0.10
+    
+    for i in range(int(5*timePeriod),0, -1):  # check from 5th harmonic
+        if abs(fourierTransform[i]) > ths:
+            high_cutoff = np.round(i/timePeriod, 1) 
+            break
+    
+    print('low cutoff: ', low_cutoff, 'high_cutoff: ', high_cutoff)
+    
+    #low_loc = np.where(frequencies == low_cutoff)[0][0]
+    #high_loc = np.where(frequencies == high_cutoff)[0][0]
+            
+    return [low_cutoff, high_cutoff]
+
+
+
+def compute_and_reconstruction_dft(data, fs, sec, overlap, cutoff):
+    concatenated_sig = []
+    
+    for i in range(0, len(data), fs*sec-overlap):
+        seg_data = data[i:i+fs*sec]
+        sig_fft = fftpack.fft(seg_data)
+    
+        
+        #The corresponding frequencies
+        sample_freq = (fftpack.fftfreq(len(seg_data)) * fs)
+
+        new_freq_fft = sig_fft.copy()
+        new_freq_fft[np.abs(sample_freq) < cutoff[0]] = 0
+        new_freq_fft[np.abs(sample_freq) > cutoff[1]] = 0
+    
+        filtered_sig = fftpack.ifft(new_freq_fft)
+        
+        # 2% overlapping
+        if i == 0:
+            concatenated_sig = np.hstack([concatenated_sig, filtered_sig[:fs*sec - overlap//2]])
+        elif i == len(data)-1:
+            concatenated_sig = np.hstack(concatenated_sig, filtered_sig[overlap//2:])
+        else:
+            concatenated_sig = np.hstack([concatenated_sig, filtered_sig[overlap//2:fs*sec - overlap//2]])
+        
+    return concatenated_sig
+
+# +
+from sklearn.preprocessing import MinMaxScaler
+
+def ppg_normalization(data, min_range, max_range):
+
+    scaler = MinMaxScaler(feature_range=(min_range,max_range))
+    scaler.fit(data)
+    norm_data = scaler.transform(data)
+    
+    return norm_data
+
+
+# +
+'''
+
+Reference:
+Hanyu, Shao, and Chen Xiaohui. "Motion artifact detection and reduction in PPG signals based on statistics analysis." 
+2017 29th Chinese control and decision conference (CCDC). IEEE, 2017.
+
+'''
+
+# cycle: per period
+
+
+def statistic_threshold(clean_signal, fs, ths):
+    stds, kurtosiss, skews, valley = statistic_detection(clean_signal, fs)
+    std_ths = np.mean(stds) + ths[0] # paper's threshold : 3.2  
+    kurt_ths = np.mean(kurtosiss) + ths[1]  #3.1  
+    skews_ths = [np.mean(skews) - ths[2], np.mean(skews) + ths[3]]  # -0.3 and 0.8   -0.5 , 0.9
+    
+    return std_ths, kurt_ths, skews_ths
+
+
+def statistic_detection(signal, fs):
+    
+    valley = pair_valley(valley_detection(signal, fs))
+    stds=[]
+    kurtosiss=[]
+    skews=[]
+
+    for val in valley: # 사이클 한 번동안의 통계적 평균 리스트 저장
+        stds.append(np.std(signal[val[0]:val[1]]))
+        kurtosiss.append(kurtosis(signal[val[0]:val[1]]))
+        skews.append(skew(signal[val[0]:val[1]])) 
+
+    return stds, kurtosiss, skews, valley
+
+def eliminate_noise_in_time(data, fs, ths,cycle=1):
+    stds, kurtosiss,skews, valley = statistic_detection(data, fs)
+    
+    
+    #cycle 수만큼 다시 평균내서 리스트 저장
+    stds_, kurtosiss_, skews_ = [], [], []
+    stds_ = [np.mean(stds[i:i+cycle]) for i in range(0,len(stds)-cycle+1,cycle)]
+    kurtosiss_ = [np.mean(kurtosiss[i:i+cycle]) for i in range(0,len(kurtosiss)-cycle+1,cycle)]
+    skews_ = [np.mean(skews[i:i+cycle]) for i in range(0,len(skews)-cycle+1,cycle)]    
+   
+    # extract clean index, 사이클 인덱스       
+    eli_std = [stds_.index(x) for x in stds_ if x < ths[0]]
+    eli_kurt = [kurtosiss_.index(x) for x in kurtosiss_ if x < ths[1]]
+    eli_skew = [skews_.index(x) for x in skews_ if x > ths[2][0] and x < ths[2][1]]
+
+    total_list = eli_std + eli_kurt + eli_skew
+    
+    
+    # store the number of extracted each index(각 인덱스 extract된 횟수 저장)
+    dic = dict()
+    for i in total_list:
+        if i in dic.keys():
+            dic[i] += 1
+        else:
+            dic[i] = 1
+            
+    new_list = []
+    for key, value in dic.items():
+        if value >= 3:
+            new_list.append(key)
+    new_list.sort()
+    
+    eliminated_data = []
+    index = []
+    for x in new_list:
+        #eliminated_data = np.hstack([eliminated_data, data[valley[x*cycle][0]:valley[x*cycle+cycle-1][1]]])
+        #valley[x*cycle][0]:valley[x*cycle+cycle-1][1]
+        #eliminated_data.extend(data[valley[x][0]:valley[x][1]])
+        index.extend([x for x in range(valley[x*cycle][0],valley[x*cycle+cycle-1][1],1)])
+
+    print(len(data), len(index))
+    return len(data), len(index), index
+
+
+# +
+def valley_detection(dataset, fs):
+    window = []
+    valleylist = []
+    ybeat = []
+    listpos = 0
+    TH_elapsed = np.ceil(0.36 * fs)
+    nvalleys = 0
+    valleyarray = []
+    
+    localaverage = np.average(dataset)
+    for datapoint in dataset:
+
+        if (datapoint > localaverage) and (len(window) < 1):
+            listpos += 1
+        elif (datapoint <= localaverage):
+            window.append(datapoint)
+            listpos += 1
+        else:
+            minimum = min(window)
+            beatposition = listpos - len(window) + (window.index(min(window)))
+            valleylist.append(beatposition)
+            window = []
+            listpos += 1
+
+    ## Ignore if the previous peak was within 360 ms interval becasuse it is T-wave
+    for val in valleylist:
+        if nvalleys > 0:
+            prev_valley = valleylist[nvalleys - 1]
+            elapsed = val - prev_valley
+            if elapsed > TH_elapsed:
+                valleyarray.append(val)
+        else:
+            valleyarray.append(val)
+            
+        nvalleys += 1    
+
+    return valleyarray
+
+
+def pair_valley(valley):
+    pair_valley=[]
+    for i in range(len(valley)-1):
+        pair_valley.append([valley[i], valley[i+1]])
+    return pair_valley
+
+
+# -
+
+def auto_correlation(filtered_3sec):
+    
+    a = plt.acorr(filtered_3sec, usevlines=True, normed=True, maxlags=(len(filtered_3sec)-1), lw=2)
+    valley_indexes = signal.argrelextrema(a[1], np.less)  # find valley index
+    plt.scatter(x=valley_indexes[0]-(len(filtered_3sec)-1), y=a[1][valley_indexes[0]], color='red', s=20)
+    
+    diff = []
+    for i in range(len(valley_indexes[0]) -1):
+        diff.append(valley_indexes[0][i+1] - valley_indexes[0][i])
+    
+    return np.average(diff)
+
+
+# +
+# it doesn't work well when they are noisy signals, a and b gotta be same
+
+'''
+def improved_moving_window_method(data, fs, mean_period):
+    
+    # find first valley
+    valley = valley_detection(data[:100],fs)
+    final_valley = [valley[0]]
+    
+    i=0
+    while True:
+        if i == 0:
+            meuw = mean_period
+            a = math.ceil(valley[0] + 2/3*meuw)
+            b = math.ceil(valley[0] + 4/3*meuw)
+        else:
+            meuw = final_valley[-1] - final_valley[-2]
+            a = math.ceil(final_valley[-1] + 2/3*meuw)
+            b = math.ceil(final_valley[-1] + 4/3*meuw)
+
+        final_valley.append(a + np.argmin(data[a:b]))      
+        i += 1   
+                        
+        if b > len(data):
+            break
+    
+    return final_valley
+# + {}
+'''
+
---
+++ preprocessing_tool/peak_detection.py
@@ -0,0 +1,484 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import scipy.signal
+
+# +
+'''
+Method 1 ) local minima and maxima
+'''
+
+def threshold_peakdetection(dataset, fs):
+    
+    #print("dataset: ",dataset)
+    window = []
+    peaklist = []
+    ybeat = []
+    listpos = 0
+    mean = np.average(dataset)
+    TH_elapsed = np.ceil(0.36 * fs)
+    npeaks = 0
+    peakarray = []
+    
+    localaverage = np.average(dataset)
+    for datapoint in dataset:
+
+        if (datapoint < localaverage) and (len(window) < 1):
+            listpos += 1
+        elif (datapoint >= localaverage):
+            window.append(datapoint)
+            listpos += 1
+        else:
+            maximum = max(window)
+            beatposition = listpos - len(window) + (window.index(max(window)))
+            peaklist.append(beatposition)
+            window = []
+            listpos += 1
+
+            
+    ## Ignore if the previous peak was within 360 ms interval becasuse it is T-wave
+    for val in peaklist:
+        if npeaks > 0:
+            prev_peak = peaklist[npeaks - 1]
+            elapsed = val - prev_peak
+            if elapsed > TH_elapsed:
+                peakarray.append(val)
+        else:
+            peakarray.append(val)
+            
+        npeaks += 1    
+
+
+    return peaklist
+
+# +
+'''
+Method 2 ) first derivative with adaptive threshold
+
+Reference:
+
+1. Li, Bing Nan, Ming Chui Dong, and Mang I. Vai. "On an automatic delineator for 
+arterial blood pressure waveforms." Biomedical Signal Processing and Control 5.1 (2010): 76-81.
+
+2. Elgendi, Mohamed, et al. "Systolic peak detection in acceleration photoplethysmograms 
+measured from emergency responders in tropical conditions." PLoS One 8.10 (2013).
+
+'''
+
+def seperate_division(data,fs):
+    divisionSet = []
+    for divisionUnit in range(0,len(data)-1,5*fs):  # index of groups (per 5sec) 
+        eachDivision = data[divisionUnit: (divisionUnit+1) * 5 * fs]
+        divisionSet.append(eachDivision)
+    return divisionSet
+
+def first_derivative_with_adaptive_ths(data, fs):
+    
+    peak = []
+    divisionSet = seperate_division(data, fs)
+    selectiveWindow = 2 * fs
+    block_size = 5 * fs
+    bef_idx = -300
+    
+    for divInd in range(len(divisionSet)):
+        block = divisionSet[divInd]
+        ths = np.mean(block[:selectiveWindow]) # ths: 2 seconds mean in block
+        
+        firstDeriv = block[1:] - block[:-1]
+        for i in range(1,len(firstDeriv)):
+            if  firstDeriv[i] <= 0 and firstDeriv[i-1] > 0:
+                if block[i] > ths:
+                    idx = block_size*divInd + i
+                    if idx - bef_idx > (300*fs/1000):
+                        peak.append(idx)
+                        bef_idx = idx
+                                                
+    return peak
+        
+
+
+
+# +
+'''
+Method 3: Slope sum function with an adaptive threshold
+
+Reference
+1. Jang, Dae-Geun, et al. "A robust method for pulse peak determination 
+in a digital volume pulse waveform with a wandering baseline." 
+IEEE transactions on biomedical circuits and systems 8.5 (2014): 729-737.
+
+2. Jang, Dae-Geun, et al. "A real-time pulse peak detection algorithm for 
+the photoplethysmogram." International Journal of Electronics and Electrical Engineering 2.1 (2014): 45-49.
+'''
+
+def determine_peak_or_not(prevAmp, curAmp, nextAmp):
+    if prevAmp < curAmp and curAmp >= nextAmp:
+        return True
+    else:
+        return False
+    
+def onoff_set(peak, sig):     # move peak from dy signal to original signal   
+    onoffset = []
+    for p in peak:
+        for i in range(p, 0,-1):
+            if sig[i] == 0:
+                onset = i
+                break
+        for j in range(p, len(sig)):
+            if sig[j] == 0:
+                offset = j
+                break
+        if onset < offset:
+            onoffset.append([onset,offset])
+    return onoffset
+    
+
+def slope_sum_function(data,fs):
+    dy = [0]
+    
+    dy.extend(np.diff(data))
+    #dy[dy < 0 ] = 0
+    
+    w = fs // 8
+    dy_ = [0] * w
+    for i in range(len(data)-w):
+        sum_ = np.sum(dy[i:i+w])
+        if sum_ > 0:
+            dy_.append(sum_)
+        else:
+            dy_.append(0)
+    
+    init_ths = 0.6 * np.max(dy[:3*fs])
+    ths = init_ths
+    recent_5_peakAmp = []
+    peak_ind = []
+    bef_idx = -300
+    
+    for idx in range(1,len(dy_)-1):
+        prevAmp = dy_[idx-1]
+        curAmp = dy_[idx]
+        nextAmp = dy_[idx+1]
+        if determine_peak_or_not(prevAmp, curAmp, nextAmp) == True:
+            if (idx - bef_idx) > (300 * fs /1000):  # Ignore if the previous peak was within 300 ms interval
+                if len(recent_5_peakAmp) < 100:  
+                    if curAmp > ths:
+                        peak_ind.append(idx)
+                        bef_idx = idx
+                        recent_5_peakAmp.append(curAmp)
+                elif len(recent_5_peakAmp) == 100:
+                    ths = 0.7*np.median(recent_5_peakAmp)
+                    if curAmp > ths:
+                        peak_ind.append(idx)
+                        bef_idx = idx
+                        recent_5_peakAmp.pop(0)
+                        recent_5_peakAmp.append(curAmp)
+                        
+    onoffset = onoff_set(peak_ind, dy_)
+    corrected_peak_ind = []
+    for onoff in onoffset:
+        segment = data[onoff[0]:onoff[1]]
+        corrected_peak_ind.append(np.argmax(segment) + onoff[0])
+                    
+    return corrected_peak_ind
+
+
+# +
+'''
+Method 4
+
+Event-Related Moving Averages with Dynamic Threshold
+
+Reference
+
+1. Elgendi, Mohamed, et al. "Systolic peak detection in acceleration photoplethysmograms 
+measured from emergency responders in tropical conditions." PLoS One 8.10 (2013).
+
+2. https://github.com/neuropsychology/NeuroKit/blob/8a2148fe477f20328d18b6da7bbb1c8438e60f18/neurokit2/signal/signal_formatpeaks.py
+
+'''
+
+def moving_average(signal, kernel='boxcar', size=5):
+    size = int(size)
+    window = scipy.signal.get_window(kernel, size)
+    w = window / window.sum()
+    
+    # Extend signal edges to avoid boundary effects
+    x = np.concatenate((signal[0] * np.ones(size), signal, signal[-1] * np.ones(size)))
+    
+    # Compute moving average
+    smoothed = np.convolve(w, x, mode='same')
+    smoothed = smoothed[size:-size]
+    return smoothed
+
+
+def moving_averages_with_dynamic_ths(signals,sampling_rate=64, peakwindow=.111, 
+                                     beatwindow=.667, beatoffset=.02, mindelay=.3,show=False):
+    if show:
+        fig, (ax0, ax1) = plt.subplots(nrow=2, ncols=1, sharex=True)
+        ax0.plot(data, label='filtered')
+    
+    signal = signals.copy()
+    # ignore the samples with n
+    signal[signal < 0] = 0
+    sqrd = signal**2
+    
+    # Compute the thresholds for peak detection. Call with show=True in order
+    # to visualize thresholds.
+    ma_peak_kernel = int(np.rint(peakwindow * sampling_rate))
+    ma_peak = moving_average(sqrd, size=ma_peak_kernel)
+    
+    ma_beat_kernel = int(np.rint(beatwindow * sampling_rate))
+    ma_beat = moving_average(sqrd, size=ma_beat_kernel)
+
+    
+    thr1 = ma_beat + beatoffset * np.mean(sqrd)    # threshold 1
+
+    if show:
+        ax1.plot(sqrd, label="squared")
+        ax1.plot(thr1, label="threshold")
+        ax1.legend(loc="upper right")
+
+    # Identify start and end of PPG waves.
+    waves = ma_peak > thr1
+    
+    beg_waves = np.where(np.logical_and(np.logical_not(waves[0:-1]),
+                                        waves[1:]))[0]
+    end_waves = np.where(np.logical_and(waves[0:-1],
+                                        np.logical_not(waves[1:])))[0]
+    # Throw out wave-ends that precede first wave-start.
+    end_waves = end_waves[end_waves > beg_waves[0]]
+
+    # Identify systolic peaks within waves (ignore waves that are too short).
+    num_waves = min(beg_waves.size, end_waves.size)
+    min_len = int(np.rint(peakwindow * sampling_rate))    # threshold 2
+    min_delay = int(np.rint(mindelay * sampling_rate))
+    peaks = [0]
+
+    for i in range(num_waves):
+
+        beg = beg_waves[i]
+        end = end_waves[i]
+        len_wave = end - beg
+
+        if len_wave < min_len: # threshold 2
+            continue
+
+        # Visualize wave span.
+        if show:
+            ax1.axvspan(beg, end, facecolor="m", alpha=0.5)
+
+        # Find local maxima and their prominence within wave span.
+        data = signal[beg:end]
+        locmax, props = scipy.signal.find_peaks(data, prominence=(None, None))
+
+        if locmax.size > 0:
+            # Identify most prominent local maximum.
+            peak = beg + locmax[np.argmax(props["prominences"])]
+            # Enforce minimum delay between peaks(300ms)
+            if peak - peaks[-1] > min_delay:
+                peaks.append(peak)
+
+    peaks.pop(0)
+
+    if show:
+        ax0.scatter(peaks, signal[peaks], c="r")
+
+    peaks = np.asarray(peaks).astype(int)
+    return peaks
+
+
+# +
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+def lmm_peakdetection(data,fs):
+    
+    peak_final = []
+    peaks, _ = find_peaks(data,height=0)
+    
+    for peak in peaks:
+        if data[peak] > 0:
+            peak_final.append(peak)
+        
+    return peak_final
+
+
+# +
+'''
+Method 5) VPD
+
+Reference
+
+Kuntamalla, Srinivas, and L. Ram Gopal Reddy. "An efficient and automatic systolic peak detection algorithm 
+for photoplethysmographic signals." International Journal of Computer Applications 97.19 (2014).
+
+'''
+
+def vpd_peakdetection2(data):
+    
+    # 3-point moving average smoothing
+    fwd = moving_average(data, size=5)
+    bwd = moving_average(data[::-1], size=5)
+    average_data = np.mean(np.vstack((fwd,bwd[::-1])), axis=0)
+    
+    
+    # find all the peaks and valleys    
+    peak, peak_idx, valley, valley_idx = [],[],[],[]
+    
+    for i in range(1, len(average_data)-1):
+        
+        # peaks: P(n) = S(n): s(n-1)<S(n)>S(n+1)
+        if (average_data[i-1] < average_data[i] and average_data[i] > average_data[i+1]):
+            peak.append(average_data[i])
+            peak_idx.append(i)
+            
+        # valleys: V(n) = S(n): S(n-1) > v< S(n+1)
+        if average_data[i-1] > average_data[i] and average_data[i] < average_data[i+1]:
+            valley.append(average_data[i])
+            valley_idx.append(i)
+     
+  
+    # if the peak comes first, discard and the signal is taken starting from the valley
+    if peak_idx[0] < valley_idx[0]:
+        peak = np.delete(peak,0)
+        peak_idx = np.delete(peak_idx,0)
+            
+    if len(peak) < len(valley):
+        valley = np.delete(valley,-1)
+        valley_idx = np.delete(valley_idx,-1)
+     
+    if len(peak) > len(valley):
+        peak = np.delete(peak,-1)
+        peak_idx = np.delete(peak_idx,-1)
+            
+    # repeat until the number of peaks in the two successive iterations remains the same
+    numOfIteration = 0
+    numOfPeaks = len(peak) # number of peaks
+    
+    while numOfIteration < 2:
+        # calculate the interval VPD(k) = P(k) - V(k)
+        vpd = []
+        for i in range(len(peak)):
+            vpd.append(peak[i] - valley[i])
+            
+        if len(vpd) < 3:
+            break
+        
+        eliminate_list = []
+       # print(len(vpd), len(peak), len(peak_idx), len(valley), len(valley_idx))
+        for i in range(1, len(vpd)-1):
+            if vpd[i] < 0.4 * (vpd[i-1] + vpd[i] + vpd[i+1]) / 3:
+                eliminate_list.append(i)
+                
+        peak = np.delete(peak,eliminate_list)
+        peak_idx = np.delete(peak_idx,eliminate_list)
+        valley = np.delete(valley,eliminate_list)
+        valley_idx = np.delete(valley_idx,eliminate_list)
+
+        numOfIteration += 1
+
+        if numOfPeaks == len(peak):
+            numOfIteration += 1
+
+        else:
+            numOfIteration = 0
+            numOfPeaks = len(peak)
+            
+    return peak_idx
+# -
+
+'''
+fs = 64
+peak = []
+
+for i in range(0,len(dy)-fs*2,fs*2):
+    th = np.mean([dy[x] for x in range(i,i+fs*2) ]) * 3
+    for j in range(i,i+fs*2,10):
+        max_ = np.argmax([dy[x] for x in range(j,j+10)]) + j
+       # print(j, max_, dy[max_])
+        if dy[max_] >= th:
+            if j == 0:
+                peak.append(max_)
+                bef_max_ = max_
+            else:
+                if (max_ - bef_max_) > (300 * fs /1000):
+                    peak.append(max_)        
+                    bef_max_ = max_
+
+print(peak[:100])
+'''
+
+
+def ensemble_peak(preprocessed_data, fs, ensemble_ths=4):
+    
+    peak1 = threshold_peakdetection(preprocessed_data,fs)
+    peak2 = slope_sum_function(preprocessed_data, fs)
+    peak3 = first_derivative_with_adaptive_ths(preprocessed_data, fs)
+    peak4 = moving_averages_with_dynamic_ths(preprocessed_data)
+    peak5 = lmm_peakdetection(preprocessed_data,fs)
+    
+    peak_dic = dict()
+
+    for key in peak1:
+        peak_dic[key] = 1
+
+    for key in peak2:
+        if key in peak_dic.keys():
+            peak_dic[key] += 1
+        else:
+            peak_dic[key] = 1
+    
+    for key in peak3:
+        if key in peak_dic.keys():
+            peak_dic[key] += 1
+        else:
+            peak_dic[key] = 1
+        
+    for key in peak4:
+        if key in peak_dic.keys():
+            peak_dic[key] += 1
+        else:
+            peak_dic[key] = 1
+        
+    for key in peak5:
+        if key in peak_dic.keys():
+            peak_dic[key] += 1
+        else:
+            peak_dic[key] = 1
+        
+    peak_dic = dict(sorted(peak_dic.items()))
+
+    count = 0
+    cnt = 0
+    bef_key = 0
+    margin = 1
+
+    new_peak_dic = dict()
+
+    for key in peak_dic.keys():
+        if cnt == 0:
+            new_peak_dic[key] = peak_dic[key]
+        else:
+            if bef_key + margin >= key:  # 마진 1안에 다음 피크가 존재하면
+                if peak_dic[bef_key] > peak_dic[key]: # 이전 피크 기준으로 개수 카운트
+                    new_peak_dic[bef_key] += peak_dic[key]
+                else:
+                    #print("new peak dic: ",new_peak_dic)
+                    new_peak_dic[key] = peak_dic[key] + peak_dic[bef_key] # 현재 피크 기준으로 개수 카운트
+                    del(new_peak_dic[bef_key])
+                    bef_key = key
+            else:
+                new_peak_dic[key] = peak_dic[key]
+                bef_key = key
+        cnt += 1
+    
+    ensemble_dic = dict()
+    for (key, value) in new_peak_dic.items():
+        if value >= ensemble_ths:
+            ensemble_dic[key] = value
+            
+    final_peak = list(ensemble_dic.keys())
+    
+    return final_peak
+
+
